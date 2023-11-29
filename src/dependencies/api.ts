8e3c0e91ac8c329da4ab705215b4bca4cdc8c952
IanMoraes <ianmoraes1@gmail.com>
2023-11-29 17:08:12 -0300

    change url

--- src/dependencies/api.ts
+++ src/dependencies/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({baseURL:'http://homologacao3.azapfy.com.br/api/ps/metahumans'})
+const api = axios.create({baseURL:'https://github.com/IanMoraes/Heroes/blob/master/heroes.json'})
 
 export default api;
